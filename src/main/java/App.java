7c666be91f590c5e12cf588c86cc621b3ce21ae7
Tonio-An <inncocentguy@gmail.com>
2020-03-25 16:31:34 +0200

    changed method sum() removed method div()

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,13 +1,12 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println(sum(10, 20));
+        System.out.println(sum(10, 20, 30));
     }
-    public static int sum(int a, int b) {
-        int c = a + b;
-        return c;
-    }
-    public static int div(int a, int b) {
-        int c = a / b;
-        return c;
+    public static int sum(int a, int b, int c) {
+        int d = a + b + c;
+        System.out.println("DONE!");
+        return d;
     }
 }
